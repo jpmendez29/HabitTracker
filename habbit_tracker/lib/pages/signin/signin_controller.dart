df3e5aedf7fe74b0bb77e45fa4ecaf27e64275dc
David Maldonado <david.maldonado@momseguros.co>
2023-05-09 02:02:19 -0500

    Update userProfile

--- habbit_tracker/lib/pages/signin/signin_controller.dart
+++ habbit_tracker/lib/pages/signin/signin_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 import 'package:habbit_tracker/pages/signin/usuarios_model.dart';
+import 'package:habbit_tracker/pages/perfil/perfil_controller.dart';
 
 class signin_controller extends GetxController {
   late final List<usuario_model> usuarios = <usuario_model>[
@@ -8,7 +9,7 @@ class signin_controller extends GetxController {
   ].obs;
 
   void setuser(us, pasw) => {
-        usuarios.add(usuario_model(user: us, pasw: pasw)),
+        usuarios.add(usuario_model(user: us, pasw: pasw, userProfile: null)),
       };
 
   String usr() {
@@ -17,8 +18,17 @@ class signin_controller extends GetxController {
     return "$user";
   }
 
+  usuario_model? currentUser;
   bool validatepasw(usr, pasw) {
     setuser(usr, pasw);
+    // Crea y guarda un nuevo perfil de usuario al registrarse.
+    usuarios.last.userProfile = UserProfile(
+      id: usuarios.last.user,
+      name: '',
+      bio: '',
+      birthdate: DateTime.now(),
+      imageUrl: '',
+    );
     return true;
   }
 
@@ -26,6 +36,7 @@ class signin_controller extends GetxController {
     try {
       final usexist = usuarios.singleWhere((element) => element.user == usr);
       if (usexist.pasw == pasw) {
+        currentUser = usexist;
         return true;
       }
     } catch (e) {
